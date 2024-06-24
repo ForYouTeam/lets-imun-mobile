10db9062eb3814630c72412aba516cd6f43b6ac8
IPKIN9 <irwandirini@gmail.com>
2024-06-24 10:11:08 +0700

    Fixed some depend not installed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,134 +7,133 @@ import { getProfile } from "@/services/profile";
 import { IResponseData } from "@/services/type";
 
 const TabsLayout = () => {
-    const [isComponentMounted, setIsComponentMounted] = useState(false);
-    const { isAuthenticated, setAuthenticated } = useGlobal();
-    const navigation = useNavigation();
+  const [isComponentMounted, setIsComponentMounted] = useState(false);
+  const { isAuthenticated, setAuthenticated } = useGlobal();
+  const navigation = useNavigation();
 
-    const fetchingData = async () => {
-        const { data, error } = await getProfile();
-        if (error) {
-            console.log(error);
-            return;
-        }
+  const fetchingData = async () => {
+    const { data, error } = await getProfile();
+    if (error) {
+      console.log(error);
+      return;
+    }
 
-        if (data) {
-            const dataResponse: IResponseData = data;
-            if (dataResponse.status != 200) {
-                setAuthenticated(true);
-            }
-        }
+    if (data) {
+      const dataResponse: IResponseData = data;
+      if (dataResponse.status != 200) {
+        setAuthenticated(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setIsComponentMounted(true);
+
+    fetchingData();
+
+    const handleBackPress = () => {
+      minimizeApp(); // Panggil fungsi minimize ketika tombol kembali ditekan
+      return true; // Event tidak menyebar
     };
 
-    useEffect(() => {
-        setIsComponentMounted(true);
+    BackHandler.addEventListener("hardwareBackPress", handleBackPress);
 
-        fetchingData();
-
-        const handleBackPress = () => {
-            minimizeApp(); // Panggil fungsi minimize ketika tombol kembali ditekan
-            return true; // Event tidak menyebar
-        };
-
-        BackHandler.addEventListener("hardwareBackPress", handleBackPress);
-
-        return () => {
-            BackHandler.removeEventListener(
-                "hardwareBackPress",
-                handleBackPress
-            );
-        };
-    }, []);
-
-    useEffect(() => {
-        if (isComponentMounted && !isAuthenticated) {
-            router.push("login"); // Gunakan navigation.push untuk navigasi ke halaman login setelah komponen terpasang
-        }
-    }, [isComponentMounted, isAuthenticated, navigation]);
-
-    const minimizeApp = () => {
-        BackHandler.exitApp();
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleBackPress);
     };
+  }, []);
 
-    return (
-        <Tabs>
-            <Tabs.Screen
-                name="index"
-                options={{
-                    headerShown: false,
-                    title: "Beranda",
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            style={styles.icon}
-                            source={
-                                focused
-                                    ? require("@/assets/images/tabbar/home.png")
-                                    : require("@/assets/images/tabbar/home-inactive.png")
-                            }
-                        />
-                    ),
-                    tabBarStyle: styles.tabBar,
-                    tabBarInactiveTintColor: "#C7C8CC",
-                    tabBarActiveTintColor: "#54808C",
-                }}
-            />
-            <Tabs.Screen
-                name="reportPanel"
-                options={{
-                    headerShown: false,
-                    title: "Laporan",
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            style={styles.icon}
-                            source={
-                                focused
-                                    ? require("@/assets/images/tabbar/news.png")
-                                    : require("@/assets/images/tabbar/news-inactive.png")
-                            }
-                        />
-                    ),
-                    tabBarStyle: styles.tabBar,
-                    tabBarInactiveTintColor: "#C7C8CC",
-                    tabBarActiveTintColor: "#54808C",
-                    tabBarItemStyle: { paddingTop: 2 },
-                }}
-            />
-            <Tabs.Screen
-                name="settingUser"
-                options={{
-                    headerShown: false,
-                    title: "Akun",
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            style={styles.icon}
-                            source={
-                                focused
-                                    ? require("@/assets/images/tabbar/account.png")
-                                    : require("@/assets/images/tabbar/account-inactive.png")
-                            }
-                        />
-                    ),
-                    tabBarStyle: styles.tabBar,
-                    tabBarInactiveTintColor: "#C7C8CC",
-                    tabBarActiveTintColor: "#54808C",
-                    tabBarItemStyle: { paddingTop: 2 },
-                }}
-            />
-        </Tabs>
-    );
+  useEffect(() => {
+    if (isComponentMounted && !isAuthenticated) {
+      router.push("login"); // Gunakan navigation.push untuk navigasi ke halaman login setelah komponen terpasang
+    }
+  }, [isComponentMounted, isAuthenticated, navigation]);
+
+  const minimizeApp = () => {
+    BackHandler.exitApp();
+  };
+
+  return (
+    isAuthenticated && (
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            headerShown: false,
+            title: "Beranda",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                style={styles.icon}
+                source={
+                  focused
+                    ? require("@/assets/images/tabbar/home.png")
+                    : require("@/assets/images/tabbar/home-inactive.png")
+                }
+              />
+            ),
+            tabBarStyle: styles.tabBar,
+            tabBarInactiveTintColor: "#C7C8CC",
+            tabBarActiveTintColor: "#54808C",
+          }}
+        />
+        <Tabs.Screen
+          name="reportPanel"
+          options={{
+            headerShown: false,
+            title: "Laporan",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                style={styles.icon}
+                source={
+                  focused
+                    ? require("@/assets/images/tabbar/news.png")
+                    : require("@/assets/images/tabbar/news-inactive.png")
+                }
+              />
+            ),
+            tabBarStyle: styles.tabBar,
+            tabBarInactiveTintColor: "#C7C8CC",
+            tabBarActiveTintColor: "#54808C",
+            tabBarItemStyle: { paddingTop: 2 },
+          }}
+        />
+        <Tabs.Screen
+          name="settingUser"
+          options={{
+            headerShown: false,
+            title: "Akun",
+            tabBarIcon: ({ focused }) => (
+              <Image
+                style={styles.icon}
+                source={
+                  focused
+                    ? require("@/assets/images/tabbar/account.png")
+                    : require("@/assets/images/tabbar/account-inactive.png")
+                }
+              />
+            ),
+            tabBarStyle: styles.tabBar,
+            tabBarInactiveTintColor: "#C7C8CC",
+            tabBarActiveTintColor: "#54808C",
+            tabBarItemStyle: { paddingTop: 2 },
+          }}
+        />
+      </Tabs>
+    )
+  );
 };
 
 export default TabsLayout;
 
 const styles = StyleSheet.create({
-    tabBar: {
-        backgroundColor: "white",
-        height: 60,
-        paddingBottom: 6,
-        paddingTop: 6,
-    },
-    icon: {
-        width: 28,
-        height: 28,
-    },
+  tabBar: {
+    backgroundColor: "white",
+    height: 60,
+    paddingBottom: 6,
+    paddingTop: 6,
+  },
+  icon: {
+    width: 28,
+    height: 28,
+  },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import {
-    DarkTheme,
-    DefaultTheme,
-    ThemeProvider,
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack, router } from "expo-router";
@@ -23,107 +23,91 @@ SplashScreen.preventAutoHideAsync();
 // Request required permissions
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-    const [loaded] = useFonts({
-        MontserratBold: require("../assets/fonts/Montserrat-Bold.ttf"),
-        MontserratRegular: require("../assets/fonts/Montserrat-Regular.ttf"),
-        MontserratMedium: require("../assets/fonts/Montserrat-Medium.ttf"),
-        MontserratSemiBold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
-        InterBold: require("../assets/fonts/Inter-Bold.ttf"),
-        InterMedium: require("../assets/fonts/Inter-Medium.ttf"),
-        InterRegular: require("../assets/fonts/Inter-Regular.ttf"),
-    });
-    const [isAppReady, setIsAppReady] = useState(false);
-    const [mount, setMount] = useState(false);
+  const [loaded] = useFonts({
+    MontserratBold: require("../assets/fonts/Montserrat-Bold.ttf"),
+    MontserratRegular: require("../assets/fonts/Montserrat-Regular.ttf"),
+    MontserratMedium: require("../assets/fonts/Montserrat-Medium.ttf"),
+    MontserratSemiBold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    InterBold: require("../assets/fonts/Inter-Bold.ttf"),
+    InterMedium: require("../assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("../assets/fonts/Inter-Regular.ttf"),
+  });
+  const [isAppReady, setIsAppReady] = useState(false);
+  const [mount, setMount] = useState(false);
 
-    Notifications.setNotificationHandler({
-        handleNotification: async () => ({
-            shouldShowAlert: true,
-            shouldPlaySound: true,
-            shouldSetBadge: true,
-        }),
-    });
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
 
-    const [notification, setNotification] = useState<
-        Notifications.Notification | undefined
-    >(undefined);
-    const notificationListener = useRef<Notifications.Subscription>();
-    const responseListener = useRef<Notifications.Subscription>();
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
 
-    const [isAuth, setAuth] = useState(false);
-    useEffect(() => {
-        setMount(true);
-        getFcmToken();
+  const [isAuth, setAuth] = useState(false);
+  useEffect(() => {
+    setMount(true);
+    getFcmToken();
 
-        if (loaded) {
-            SplashScreen.hideAsync();
-            notificationListener.current =
-                Notifications.addNotificationReceivedListener(
-                    (notification) => {
-                        setNotification(notification);
-                        console.log(notification);
-                    }
-                );
+    if (loaded) {
+      SplashScreen.hideAsync();
+      notificationListener.current =
+        Notifications.addNotificationReceivedListener((notification) => {
+          setNotification(notification);
+          console.log(notification);
+        });
 
-            responseListener.current =
-                Notifications.addNotificationResponseReceivedListener(
-                    (response) => {
-                        console.log(response);
-                    }
-                );
+      responseListener.current =
+        Notifications.addNotificationResponseReceivedListener((response) => {
+          console.log(response);
+        });
 
-              getToken().then(({data, error}) => {
-                if (error) {
-                    console.log(error);
-                    setAuth(false);
-                }
-                if (data && data.length > 0) {
-                    setAuth(true);
-                } else {
-                    setAuth(false);
-                }
-              })
-              .catch((e) => {console.log(e);
-              });    
-        }
-    }, [loaded]);
-
-    useEffect(() => {
-        setTimeout(() => {
-            HandlerNotification();
-            setIsAppReady(true);
-            if (!isAuth && mount) {
-                router.push("login");
-            }
-        }, 3000);
-    }, [isAuth, mount]);
-
-    if (!loaded || !isAppReady) {
-        return <CustomSplashScreen />;
+      getToken()
+        .then(({ data, error }) => {
+          if (error) {
+            console.log(error);
+            setAuth(false);
+          }
+          if (data && data.length > 0) {
+            setAuth(true);
+          } else {
+            setAuth(false);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     }
+  }, [loaded]);
 
-    return (
-        <GlobalProvider>
-            <ThemeProvider
-                value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-            >
-                <Stack>
-                    <Stack.Screen
-                        name="(tabs)"
-                        options={{ headerShown: false }}
-                    />
-                    <Stack.Screen name="+not-found" />
-                    <Stack.Screen
-                        name="login"
-                        options={{ headerShown: false }}
-                    />
-                     <Stack.Screen
-                        name="register"
-                        options={{ headerShown: false }}
-                    />
-                </Stack>
-            </ThemeProvider>
-        </GlobalProvider>
-    );
+  useEffect(() => {
+    setTimeout(() => {
+      HandlerNotification();
+      setIsAppReady(true);
+    }, 3000);
+  }, [isAuth, mount]);
+
+  if (!loaded || !isAppReady) {
+    return <CustomSplashScreen />;
+  }
+
+  return (
+    <GlobalProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
+          <Stack.Screen name="register" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </GlobalProvider>
+  );
 }
